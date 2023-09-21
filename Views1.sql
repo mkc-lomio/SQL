002ca61d378f036555482a9f6a2048e4f7f91923
Marc Kenneth Capitulo-Lomio <marckenneth.lomio@gmail.com>
2023-09-22 00:24:38 +0800

    [ADD] Views

---
+++ Views1.sql
@@ -0,0 +1,38 @@
+-- creating view
+CREATE VIEW sales.product_info
+AS
+SELECT
+    product_name, 
+    brand_name, 
+    list_price
+FROM
+    production.products p
+INNER JOIN production.brands b 
+        ON b.brand_id = p.brand_id;
+        
+-- modifying view        
+ALTER VIEW sales.product_info
+AS
+SELECT
+    product_name, 
+    brand_name, 
+    list_price
+FROM
+    production.products p
+INNER JOIN production.brands b 
+        ON b.brand_id = p.brand_id;
+        
+        
+ -- GET VIEW INFORMATION
+ SELECT
+    definition,
+    uses_ansi_nulls,
+    uses_quoted_identifier,
+    is_schema_bound
+FROM
+    sys.sql_modules
+WHERE
+    object_id
+    = object_id(
+            'sales.daily_sales'
+        );
